f20324e3489b7e0bdd72dd316a67f21cfd413fb6
kosorio2 <67075183+kosorio2@users.noreply.github.com>
2021-07-13 21:39:05 -0600

    Update upDown.cs

--- upDown.cs
+++ upDown.cs
@@ -1,1 +1,105 @@
 
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+​
+
+public class upDown : MonoBehaviour
+
+{
+
+    float speed = 5f;
+
+    bool change = true;
+
+    private SpriteRenderer spriteRenderer;
+
+    public float startPosition;
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        startPosition = GameObject.FindGameObjectWithTag("EnemyUpDown").transform.position.y;
+
+    }
+
+​
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        if (change)
+
+        {
+
+            moveObjectUp();
+
+        }
+
+        if (!change)
+
+        {
+
+            moveObjectDown();
+
+        }
+
+        if (GameObject.FindGameObjectWithTag("EnemyUpDown").transform.position.y >= startPosition + 3f)
+
+        {
+
+            change = false;
+
+            spriteRenderer.flipY = true;
+
+        }
+
+        if (GameObject.FindGameObjectWithTag("EnemyUpDown").transform.position.y <= startPosition - 3f)
+
+        {
+
+            change = true;
+
+            spriteRenderer.flipY = false;
+
+        }
+
+​
+
+    
+
+        }
+
+        void moveObjectUp()
+
+        {
+
+            transform.Translate(0, speed * Time.deltaTime, 0);
+
+        }
+
+        void moveObjectDown()
+
+        {
+
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+
+        }
+
+}
+
+​
+
+
